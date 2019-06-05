5dbd993649be3456c87b1b276e5d6ec8ec2b5baf
Thibdr <thibault.informatique@gmail.com>
2019-06-05 19:24:19 +0200

    add component to start work

--- HarvestLive/src/app/app.module.ts
+++ HarvestLive/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapPageComponent } from './map-page/map-page.component';
+import { AddPageComponent } from './add-page/add-page.component';
+import { ComparePageComponent } from './compare-page/compare-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapPageComponent,
+    AddPageComponent,
+    ComparePageComponent
   ],
   imports: [
     BrowserModule
