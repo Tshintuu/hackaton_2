03246d7264faad04fcf2f703529a15bd8a1ea38c
Aurélien Grenet <aurel.grenet@gmail.com>
2019-06-06 01:51:34 +0200

    Adding the leaflet's map

--- HarvestLive/src/app/map/map.component.ts
+++ HarvestLive/src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as L from 'leaflet';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -8,8 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class MapComponent implements OnInit {
 
   constructor() { }
-
+  public myharvestMap;
   ngOnInit() {
+
+    this.myharvestMap = L.map('harvestMap').setView([	47, 2], 6.4);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'Harvest Map'
+
+  }).addTo(this.myharvestMap);
+  /*L.marker([48.864716, 2.349014]).addTo(this.myharvestMap)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();*/
+
+
   }
 
 }
