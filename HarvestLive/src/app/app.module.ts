daadda46c1999f2e0ad664f90503cec0b8a0683f
Thibdr <thibault.informatique@gmail.com>
2019-06-06 02:45:16 +0200

    create footer, navbar, applying color chart

--- HarvestLive/src/app/app.module.ts
+++ HarvestLive/src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MapPageComponent } from './map-page/map-page.component';
 import { AddPageComponent } from './add-page/add-page.component';
 import { ComparePageComponent } from './compare-page/compare-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapPageComponent,
     AddPageComponent,
-    ComparePageComponent
+    ComparePageComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: "",
+          component: MapPageComponent
+        },
+        {
+          path: "ajouterResultats",
+          component: AddPageComponent
+        },
+        {
+          path: "comparerResultats",
+          component: ComparePageComponent
+        }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
